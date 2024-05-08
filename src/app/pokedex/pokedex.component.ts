be409a5f3c65e78dc535bac8874a5c06e47444cd
VladimirInnokentiev <vladimir.innokentyev@yaknet.com>
2024-05-08 18:23:16 +0900

    upd: id and type

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -17,10 +17,15 @@ export class PokedexComponent {
   img: string = "";
 
   search(id: any){
-    this.http.get('https://pokeapi.co/api/v2/pokemon/' + id).subscribe((response)=>{
-      this.response = response;
-      this.img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + id + ".png"; 
-    })
+      this.http.get('https://pokeapi.co/api/v2/pokemon/' + id).subscribe((response)=>{
+        this.response = response;
+        if (this.isNumeric(id)){
+          this.img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + id + ".png";
+        }
+        else{
+          this.img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + this.response.id + ".png"; 
+        };
+      })
   }
 
   searchPrev(id: any){
@@ -33,4 +38,23 @@ export class PokedexComponent {
     this.search(id);
   }
 
+  getId(){
+      return this.response.id;
+  }
+
+  getName(){
+    return this.response.name[0].toUpperCase()+this.response.name.slice(1);
+  }
+
+  getType(){
+    const types = [];
+    for(let i = 0; i < this.response.types.length; i++){
+      types.push(this.response.types[i].type.name);
+    } 
+    return types.join(', ');
+  }
+
+  isNumeric(value: any){
+    return /^-?\d+$/.test(value);
+  }
 }
