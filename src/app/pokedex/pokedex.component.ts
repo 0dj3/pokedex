751978cd9d78a187672e11bd474c348760f2489e
VladimirInnokentiev <vladimir.innokentyev@yaknet.com>
2024-05-24 17:44:03 +0900

    Added species and stats, updated UI

--- src/app/pokedex/pokedex.component.ts
+++ src/app/pokedex/pokedex.component.ts
@@ -13,10 +13,14 @@ export class PokedexComponent {
 
   pokemonId: string = "";
   response: any;
+  species: any;
   artwork: any;
   img: string = "";
 
   search(id: any){
+    this.http.get('https://pokeapi.co/api/v2/pokemon-species/' + id).subscribe((species)=>{
+      this.species = species;
+    })
       this.http.get('https://pokeapi.co/api/v2/pokemon/' + id).subscribe((response)=>{
         this.response = response;
         if (this.isNumeric(id)){
@@ -56,5 +60,21 @@ export class PokedexComponent {
 
   isNumeric(value: any){
     return /^-?\d+$/.test(value);
+  }
+
+  getHp(){
+    return this.response.stats[0].base_stat;
+  }
+
+  getAttack(){
+    return this.response.stats[1].base_stat;
+  }
+
+  getDefense(){
+    return this.response.stats[2].base_stat;
+  }
+
+  getSpecies(){
+    return this.species.flavor_text_entries[1].flavor_text;
   }
 }
